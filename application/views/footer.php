98b0c1bfa5bc381c5d644e18d2a33fc318dcf510
alexorozco2410 <47190308+alexorozco2410@users.noreply.github.com>
2019-10-11 01:51:56 -0500

    modificacion 11/09/2019

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,22 +4,36 @@
     <footer class="page-footer font-small my-footer ">
       <div class="container-fluid text-md-left cont-footer">
         <div class="row align-items-center justify-content-around">
-          <div class="col-md-5 mt-md-0 mt-3">
+          <div class="col-md-6 mt-md-0 mt-3">
             <h6 class="tit-footer">Universidad Nacional Autónoma de México</h6>
             <h6 class="tit-footer" id="Facultad">Facultad de Ingeniería</h6>
           </div>
-          <div class="col-md-5 mt-md-0 mt-3">
+          <div class="col-md-6 mt-md-0 mt-3">
             <p class="inf-footer">División de Ingenierías Civil y Geomática</p>
             <p class="inf-footer">Circuito Exterior s/n Col. Ciudad Universitaria C.P.
               04510 Del. Coyoacán México D.F. 56228005 ext. 1037</p>
           </div>
-          <div class="col-md-2" style="text-align:center;">
-            <!-- Social buttons -->
-
+        </div>
+        <div class="row align-items-center justify-content-around">
+          <div class="container-fluid">
+            <ul class="list-inline text-center list-unstyled mb-0">
+              <li class="list-inline-item text-center">
+                <a href="#" class="fa fa-facebook"></a>
+              </li>
+              <li class="list-inline-item text-center">
+                <a href="#" class="fa fa-twitter"></a>
+              </li>
+              <li class="list-inline-item text-center">
+                <a href="#" class="fa fa-rss"></a>
+              </li>
+              <li class="list-inline-item text-center">
+                <a href="#" class="fa fa-youtube"></a>
+              </li>
+            </ul>
           </div>
         </div>
         <div class="row justify-content-center">
-          <p class="cr-style"> © 2019 Copyright</p>
+          <p class="cr-style mb-0"> © 2019 Copyright</p>
         </div>
       </div>
     </footer>
--- application/views/historial.php
+++ application/views/historial.php
@@ -2,24 +2,45 @@
 <link rel="stylesheet" href="../assets/css/historial.css">
 
 <div class="container-fluid page-container tabh-style">
+  <!--Titulo de la pagina-->
   <div class="row justify-content-center">
     <div class="col-md-12 col-sm-12 col-xs-12">
-      <h3 class="titulo-h mb-5">Historial</h3>
+      <h3 class="titulo-h mb-4">Historial</h3>
+    </div>
+  </div>
+  <!--para filtrar por estado de soporte-->
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <label for="" class="mb-0">
+        <div class="btn-group">
+          <button type="button" class="btn btn-primary">Todos</button>
+          <button type="button" class="btn btn-primary">Terminado</button>
+          <button type="button" class="btn btn-primary">Pendiente</button>
+          <button type="button" class="btn btn-primary">En Proceso</button>
+        </div>
+      </label>
+    </div>
+  </div>
+  <!--Tabla de historial de soportes-->
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-sx-12">
       <div class="table-responsive">
         <table class="table ">
           <thead class="thead-dark">
             <tr>
               <th class="atr-hist">Folio</th>
-              <th class="atr-hist">Integrante UC</th>
+              <th class="atr-hist">Equipo</th>
               <th class="atr-hist">Usuario</th>
               <th class="atr-hist">Cubículo/Salón</th>
+              <th class="atr-hist">Integrante UC</th>
               <th class="atr-hist">Fecha y Hora Inicio</th>
               <th class="atr-hist">Fecha Y Hora Final</th>
-              <th class="atr-hist">Solución</th>
+              <th class="atr-hist">Avance</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr onclick="location='solicitud_controller'">
+              <td>44444</td>
               <td>44444</td>
               <td>44444</td>
               <td>44444</td>
